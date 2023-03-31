f644c8b97347a013701c9ec058eb80ef264f4074
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-03-31 22:08:55 +0300

    first commit

---
+++ functions/math.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    answer=a+b
+    return answer
+def subtraction(c,d):
+    result=c-d
+    return result
+def division(y,p):
+    div=y/p
+    return div
+def multiplication(t,r):
+    multipy=t*r
+    return multipy
+def remainder(g,m):
+    word=g%m
+    return word
